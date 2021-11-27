bec77cd430de32b89a5cc88e14753ef3acc2ef29
EstAk <aguiklein@hotmail.com>
2021-11-27 02:44:29 +0100

    significantly improving compiling speed

--- candy_remake/const.h
+++ candy_remake/const.h
@@ -7,10 +7,12 @@
 const int windowWidth=550;
 const int windowHeight=550;
 const double refreshPerSecond=60;
-std::string maps[3]={"board0.txt","board1.txt","board2.txt"}; //[2] changed -> number of maps.
+const std::string maps[3]={"boards/board0.txt","boards/board1.txt","boards/board2.txt"}; //[2] changed -> number of maps.
 const std::string score_file = "score_file.txt";
-const char *w = "W";
-const char *c = "C";
-const char *i = "I";
-
+const char* const w = "W";
+const char* const c = "C";
+const char* const i = "I";
+//citation form stack overflow const char *constant is not const. It is a non-const pointer to const char. 
+//Being non-const variable in namespace scope, it has external linkage by default and thus you get multiple definition error.
+//const char * const constant is const and would behave as you are expecting.
 #endif
